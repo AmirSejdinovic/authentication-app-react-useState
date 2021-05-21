665a556f2830237143925fff72ff05d5ae8007e4
Amir Sejdinovic <amir24V@yahoo.com>
2021-05-21 10:43:53 +0200

    Addin the UI of app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import aut from './aut.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+     <h1>Authentication Portal</h1>
+      <div className="row">
+        <div className="col-md-6">
+          <img className="float-start" src={aut} width="100%"/>
+        </div>
+        <div className="col-md-6 my-auto bg-dark p-5 rounded">
+        <form>
+  <div className="form-group">
+    <label >Email address</label>
+    <input type="email" className="form-control"  placeholder="Enter email"/>
+   
+  </div>
+  <div class="form-group">
+    <label >Password</label>
+    <input type="password" className="form-control"  placeholder="Password"/>
+  </div>
+  
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+        </div>
+
+      </div>
     </div>
+
+
   );
 }
 
