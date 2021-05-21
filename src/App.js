2dbd99d3836ecc4716423214c539c85f3a18c66c
Amir Sejdinovic <amir24V@yahoo.com>
2021-05-21 11:03:48 +0200

    adding logic for form submit via useState method

--- src/App.js
+++ src/App.js
@@ -2,8 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import aut from './aut.png';
+//Importing react and method useState
+import React, {useState} from 'react';
 
 function App() {
+  //Creating varaible and method inside useState react hook
+  const [user,setUser] = useState('');
+  const [pass,setPass] = useState('');
+  //Creating custom function for form sumbition. 
+  const handleValidate=(e)=>{
+    //method for preventihg the reload of page
+    e.preventDefault();
+      //Condition if is right email and password than show succes message
+       if(user== 'react@gmail.com' && pass == 123456)
+       alert("Successuful login")
+       //If is not sucess than show alert of eror
+       else
+       alert("Wrong credentials")
+  }
   return (
     <div className="App-header">
      <h1>Authentication Portal</h1>
@@ -12,21 +28,27 @@ function App() {
           <img className="float-start" src={aut} width="100%"/>
         </div>
         <div className="col-md-6 my-auto bg-dark p-5 rounded">
-        <form>
+        <form onSubmit={handleValidate}>
   <div className="form-group">
     <label >Email address</label>
-    <input type="email" className="form-control"  placeholder="Enter email"/>
+    <input type="email" className="form-control"  placeholder="Enter email"
+    value={user} onChange={(e)=>setUser(e.target.value)}/>
    
   </div>
-  <div class="form-group">
+  <div className="form-group">
     <label >Password</label>
-    <input type="password" className="form-control"  placeholder="Password"/>
+    <input type="password" className="form-control"  placeholder="Password"
+    value={pass} onChange={(e)=>setPass(e.target.value)}/>
   </div>
   
   <button type="submit" className="btn btn-primary">Submit</button>
 </form>
-        </div>
 
+      <p>Email: react@gmail.com</p>
+      <p>Password: 123456</p>
+        </div>
+      
+      
       </div>
     </div>
 
